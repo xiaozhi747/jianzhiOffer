62d259be79a66a61804a248f0ca0aebe3b9ad3c4
linzhi <739769483@qq.com>
2018-05-15 16:29:19 +0800

    Add solution

---
+++ src/com/jianzhiOffer/No9.java
@@ -0,0 +1,28 @@
+package com.jianzhiOffer;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ *
+ * Created by 林智 on 2018/5/15.
+ */
+public class No9 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(stack1.isEmpty() && stack2.isEmpty()) {
+            throw new RuntimeException("Queue is empty!");
+        } else if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
