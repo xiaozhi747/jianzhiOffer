a5767eda7dc52ea8e44197c6755cce5b4a51c545
linzhi <739769483@qq.com>
2018-05-27 22:55:02 +0800

    add expansion

--- src/com/jianzhiOffer/No32.java
+++ src/com/jianzhiOffer/No32.java
@@ -7,6 +7,9 @@ import java.util.Queue;
 /**
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  *
+ * 拓展一 : 在 Print() 中实现
+ * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+ *
  * Created by 林智 on 2018/5/25.
  */
 public class No32 {
@@ -38,6 +41,38 @@ public class No32 {
             }
         }
         return list;
+    }
+
+    ArrayList<ArrayList<Integer> > Print(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int nextLevel = 0;  // 下一层节点的数目
+        int toBePrint = 1;  // 当前层中还未打印的节点数
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+                nextLevel++;
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+                nextLevel++;
+            }
+            toBePrint--;
+            if (toBePrint == 0) {
+                result.add(list);
+                list = new ArrayList<Integer>();
+                toBePrint = nextLevel;
+                nextLevel = 0;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
