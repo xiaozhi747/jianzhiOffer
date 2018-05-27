9503ff134892da650cc91deab8e0431b518fb243
linzhi <739769483@qq.com>
2018-05-27 23:40:48 +0800

    add expansion

--- src/com/jianzhiOffer/No32.java
+++ src/com/jianzhiOffer/No32.java
@@ -3,12 +3,19 @@ package com.jianzhiOffer;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
  *
  * 拓展一 : 在 Print() 中实现
  * 从上到下按层打印二叉树，同一层结点从左至右输出。每一层输出一行。
+ *
+ * 拓展二 : 在 Print2() 中实现
+ * 请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，
+ * 第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+ *
+ * 三个题目的思路都很相似
  *
  * Created by 林智 on 2018/5/25.
  */
@@ -74,6 +81,57 @@ public class No32 {
         }
         return result;
     }
+
+    public ArrayList<ArrayList<Integer>> Print2(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> oddStack = new Stack<TreeNode>();  // 当遍历到奇数层时用这个栈
+        Stack<TreeNode> evenStack = new Stack<TreeNode>(); // 当遍历到偶数层时用这个栈
+        int levelNumber = 1;
+        int nextLevelNodeNumber = 0;
+        int toBePrint = 1;  // 当前层中还未打印的节点数
+        oddStack.add(root);
+        while (!oddStack.isEmpty() || !evenStack.isEmpty()) {
+            // 奇数层
+            if ((levelNumber & 1) == 1) {
+                TreeNode node = oddStack.pop();
+                list.add(node.val);
+                if (node.left != null) {
+                    evenStack.add(node.left);
+                    nextLevelNodeNumber++;
+                }
+                if (node.right != null) {
+                    evenStack.add(node.right);
+                    nextLevelNodeNumber++;
+                }
+                toBePrint--;
+            } else { // 偶数层
+                TreeNode node = evenStack.pop();
+                list.add(node.val);
+                if (node.right != null) {
+                    oddStack.add(node.right);
+                    nextLevelNodeNumber++;
+                }
+                if (node.left != null) {
+                    oddStack.add(node.left);
+                    nextLevelNodeNumber++;
+                }
+                toBePrint--;
+            }
+            if (toBePrint == 0) {
+                result.add(list);
+                list = new ArrayList<Integer>();
+                toBePrint = nextLevelNodeNumber;
+                nextLevelNodeNumber = 0;
+                levelNumber++;
+            }
+        }
+        return result;
+    }
+
 
     public static void main(String[] args) {
         System.out.println(PrintFromTopToBottom(null));
