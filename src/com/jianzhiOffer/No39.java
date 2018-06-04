32b0ac5ed1529032fbe8d4586f2ce43f2403969c
linzhi <739769483@qq.com>
2018-06-04 11:27:04 +0800

    add solution

---
+++ src/com/jianzhiOffer/No39.java
@@ -0,0 +1,53 @@
+package com.jianzhiOffer;
+
+/**
+ * 数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+ * 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+ * 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。
+ * 如果不存在则输出0。
+ *
+ * Created by 林智 on 2018/6/4.
+ */
+public class No39 {
+    public static int MoreThanHalfNum_Solution(int [] array) {
+        if (array == null || array.length == 0) {
+            return 0;
+        }
+        int j = 1;
+        int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (result != array[i]) {
+                if (j <= 1) {
+                    result = array[i];
+                }
+                if (j > 0) {
+                    j--;
+                }
+            } else {
+                j++;
+            }
+        }
+        if (!checkMoreThanHalf(array, result)) {
+            return 0;
+        }
+        return result;
+    }
+
+    private static boolean checkMoreThanHalf(int[] array, int number){
+        int times = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (number == array[i]) {
+                times++;
+            }
+        }
+        if (times * 2 > array.length) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,3,4,5,2,2,2,2,2};
+        System.out.println(MoreThanHalfNum_Solution(array));
+    }
+}
