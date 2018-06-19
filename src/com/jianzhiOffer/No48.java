d1b3d4043aab32c40713cc9776ed92e9e2e27727
linzhi <739769483@qq.com>
2018-06-19 16:10:45 +0800

    add comments

--- src/com/jianzhiOffer/No48.java
+++ src/com/jianzhiOffer/No48.java
@@ -20,21 +20,29 @@ public class No48 {
         if (str == null || str.length() == 0) {
             return 0;
         }
-        int distance = 0;
-        int curLength = 0;
+        int distance = 0;   // 当前位置的字符上一次出现的位置 与 当前位置的距离
+        int curLength = 0;  // 当前位置 最长不含重复字符的子字符串 的长度
         int max = 0;
         char[] chs = str.toCharArray();
+        // 用于存放字符出现的最后一个位置
         Map<Character, Integer> hashMap = new HashMap<Character, Integer>();
         for (int i = 0; i < chs.length; i++) {
+            // 如果该字符没出现过, 那么放入该字符,
+            // 并且当前位置的 最长不含重复字符的子字符串 的长度是 (上一个位置的长度 + 1)
             if (!hashMap.containsKey(chs[i])) {
                 hashMap.put(chs[i], i);
                 curLength++;
                 max = Math.max(curLength, max);
             } else {
                 distance = i - hashMap.get(chs[i]);
+                // 如果 当前位置的字符上一次出现的位置 与 当前位置的距离 大于上一个位置的长度,
+                // 那么说明这个重复字符是不影响当前位置的最大长度的
                 if (distance > curLength) {
                     curLength++;
                 } else {  // distance <= curLength
+                    // 如果 当前位置的字符上一次出现的位置 与 当前位置的距离 小于上一个位置的长度,
+                    // 那么说明这个重复字符是影响当前位置的最大长度的, 并且当前位置的最
+                    // 大长度就为距离 distance
                     curLength = distance;
                 }
                 max = Math.max(curLength, max);
