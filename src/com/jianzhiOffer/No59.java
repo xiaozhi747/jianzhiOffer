4a22c5208e9eff58357d41cda8818316a56e5842
linzhi <739769483@qq.com>
2018-06-28 20:51:19 +0800

    add solution

--- src/com/jianzhiOffer/No59.java
+++ src/com/jianzhiOffer/No59.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
- * 滑动窗口的最大值
+ * 题目一：滑动窗口的最大值
  *
  * 给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值。
  * 例如，如果输入数组{2,3,4,2,6,2,5,1}及滑动窗口的大小3，那么一共存在6个滑动窗口，
@@ -14,10 +14,20 @@ import java.util.LinkedList;
  * {2,3,[4,2,6],2,5,1}， {2,3,4,[2,6,2],5,1}，
  * {2,3,4,2,[6,2,5],1}， {2,3,4,2,6,[2,5,1]}。
  *
+ *
+ * 题目二：队列的最大值
+ *
+ * 请定义一个队列并实现函数 max 得到队列里的最大值, 要求函数 max、push_bac 和 pop_front
+ * 的时间复杂度都是 O(1)
+ *
+ *
  * Created by 林智 on 2018/6/28.
  */
 public class No59 {
 
+    /**
+     * 题目一
+     */
     public static ArrayList<Integer> maxInWindows(int [] num, int size) {
         ArrayList<Integer> res = new ArrayList<Integer>();
         if (num == null || num.length == 0 || size <= 0) {
@@ -38,6 +48,44 @@ public class No59 {
             }
         }
         return res;
+    }
+
+    LinkedList<Integer> dataQueue = new LinkedList<Integer>();
+    LinkedList<Integer> maxQueue = new LinkedList<Integer>();
+
+    /**
+     * 题目二
+     *
+     * 插入队列尾部
+     */
+    public void push_back(int number) {
+        dataQueue.addLast(number);
+        while (!maxQueue.isEmpty() && number > maxQueue.peekLast()) {
+            maxQueue.pollLast();
+        }
+        maxQueue.addLast(number);
+    }
+
+    /**
+     * 从队列头部弹出
+     */
+    public void pop_front() {
+        if (!dataQueue.isEmpty()) {
+            if (maxQueue.peekFirst() == dataQueue.peekFirst()) {
+                maxQueue.pollFirst();
+            }
+            dataQueue.pollFirst();
+        }
+    }
+
+    /**
+     * 获取队列中的最大值
+     */
+    public int max() {
+        if (!maxQueue.isEmpty()) {
+            return maxQueue.peekFirst();
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
