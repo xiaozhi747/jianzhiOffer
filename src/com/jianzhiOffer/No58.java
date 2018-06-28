6a5fa5b29e92758298a1503e44937cd7f673f55d
linzhi <739769483@qq.com>
2018-06-28 19:00:34 +0800

    add solution

--- src/com/jianzhiOffer/No58.java
+++ src/com/jianzhiOffer/No58.java
@@ -7,6 +7,13 @@ package com.jianzhiOffer;
  * 为简单起见，标点符号和普通字母一样处理。
  * 例如输入字符串 "I am a student."，则输出 "student. a am I"
  *
+ *
+ * 题目二：左旋转字符串
+ *
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+ * 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串 "abcdefg" 和数字 2，
+ * 该函数将返回左旋转两位得到的结果 "cdefgab".
+ *
  * Created by 林智 on 2018/6/26.
  */
 public class No58 {
@@ -48,6 +55,28 @@ public class No58 {
             start++;
             end--;
         }
+    }
+
+    public String LeftRotateString(String str,int n) {
+        if (str == null ) {
+            return null;
+        }
+        if (n > str.length() || str.length() == 0 || n <= 0) {
+            return str;
+        }
+        char[] chs = str.toCharArray();
+        int firstPartStart = 0;
+        int firstPartEnd = n - 1;
+        int SecondPartStart = n;
+        int SecondPartEnd = chs.length - 1;
+        reverse(chs, firstPartStart, firstPartEnd);
+        reverse(chs, SecondPartStart, SecondPartEnd);
+        reverse(chs, 0, chs.length - 1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chs.length; i++) {
+            sb.append(chs[i]);
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
